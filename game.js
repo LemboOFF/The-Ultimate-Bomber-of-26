ec76be5ac69c2661b02897863e6761ac167c0fe8
LemboOFF <waffl3gaming7@gmail.com>
2026-03-26 18:37:34 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,10 +2,35 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const player = new Image();
-player.src = "https://cdn.jsdelivr.net/gh/LemboOFF/The-Ultimate-Bomber-of-26@main/player.png";
+player.src = "https://lembooff.github.io/The-Ultimate-Bomber-of-26/player.png";
 
-player.onload = function() {
+let x = 400;
+let y = 300;
+let speed = 3;
+
+const keys = {};
+
+document.addEventListener("keydown", function(e) {
+    keys[e.key] = true;
+});
+
+document.addEventListener("keyup", function(e) {
+    keys[e.key] = false;
+});
+
+function gameLoop() {
+    if (keys["w"]) y -= speed;
+    if (keys["s"]) y += speed;
+    if (keys["a"]) x -= speed;
+    if (keys["d"]) x += speed;
+
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 800, 600);
-    ctx.drawImage(player, 400, 300);
+    ctx.drawImage(player, x, y);
+
+    requestAnimationFrame(gameLoop);
+}
+
+player.onload = function() {
+    gameLoop();
 }
