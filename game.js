60ed0b1f824c203948841230826a4cfa737e3368
LemboOFF <waffl3gaming7@gmail.com>
2026-03-26 17:54:50 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, 800, 600);
+
+ctx.fillStyle = "white";
+ctx.fillText("The Ultimate Bomber of '26", 300, 300);
